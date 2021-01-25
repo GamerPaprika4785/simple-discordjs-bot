e6f4ab34b6e940460a34953d1347e4f7538c1136
GamerPaprika4785 <fidul493@gmail.com>
2021-01-25 18:22:01 +0700

    addded some file

--- bot.js
+++ bot.js
@@ -2,12 +2,16 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
 })
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "XXXXXXXXXXX"
-
-client.login(bot_secret_token)
+client.login("owoowosomekeyuseyourowntokensmhMGwe7Y") // Replace XXXXX with your bot token
